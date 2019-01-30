4d4d23f8c95568eeac1908eedf458c3f4c5ee2ea
Dustin Chad Parker <dustin.chad.parker@gmail.com>
2019-01-30 11:56:16 -0600

    took comment out

--- apps.js
+++ apps.js
@@ -49,39 +49,3 @@ let whateverYoudLike = (name1) => {
 
 let myName = "Charlie";
 whateverYoudLike(myName);
-
-
-
-// let fruits = ["apples", "oranges", "pineapples"];
-// let leastFavFruit = fruits[1];
-
-// function firstFruit () {
-
-//     let faveFruit = fruits[2];
-//     console.log(fruits[0]);
-    
-//     favFruit();
-    
-//     function favFruit() {
-//         console.log(faveFruit);
-//     }
-    
-    
-//     console.log(leastFavFruit);
- 
-
-// }
-
-// whateverILike();
-// function whateverILike() {
-//     console.log("Hello, Chad.");
-// }
-
-
-// let newFunc = function() {
-//     alert("Some text of my choosing.");
-
-// }
-// newFunc();
-
-// firstFruit();
